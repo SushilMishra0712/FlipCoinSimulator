d3b07ada5b7afc816328ddfc63330ac8eb31bfd9
Sushil <SushilMishra0712>
2019-11-20 14:52:28 +0530

    stored the doublet combination and found percentage for the same

--- flipCoinSimulator.php
+++ flipCoinSimulator.php
@@ -1,7 +1,9 @@
 <?php
 
-$heads=0;
-$tails=0;
+$head_head=0;
+$head_tail=0;
+$tail_head=0;
+$tail_tail=0;
 
 echo "How many times do you want to flip a coin?\n";
 fscanf(STDIN,"%d",$no_of_times_flip);
@@ -14,21 +16,56 @@ for($count=0;$count<$no_of_times_flip;$count++)
     if($random==1)
     {
         echo "Head\n";
-        $heads++;
+        echo "Press Enter to flip coin";
+        fscanf(STDIN,"%s");
+        $random = rand(0,1);
+        if($random==1)
+        {
+            echo "HeadHead\n";
+            $head_head++;
+            
+        }
+        else
+        {
+            echo "HeadTail\n";
+            $head_tail++;
+            
+        }
+        
     }
     else
     {
         echo "Tail\n";
-        $tails++;
+        echo "Press Enter to flip coin";
+        fscanf(STDIN,"%s");
+        $random = rand(0,1);
+        if($random==1)
+        {
+            echo "TailHead\n";
+            $tail_head++;
+            
+        }
+        else
+        {
+            echo "TailTail\n";
+            $tail_tail++;
+            
+        }
+        
     }
+    
 }
 
-$dictionary = array("Head"=>$heads,"Tail"=>$tails);
+$dictionary = array("HH"=>$head_head,"HT"=>$head_tail,"TH"=>$tail_head,"TT"=>$tail_tail);
 print_r($dictionary);
 
-$percentage_heads = ($heads/$no_of_times_flip)*100;
-echo "Percentage of heads:".$percentage_heads."%\n";
-$percentage_tails = ($tails/$no_of_times_flip)*100;
-echo "Percentage of tails:".$percentage_tails."%\n";
+$percentage_head_head = ($head_head/$no_of_times_flip)*100;
+echo "Percentage of HH:".$percentage_head_head."%\n";
+$percentage_head_tail = ($head_tail/$no_of_times_flip)*100;
+echo "Percentage of HT:".$percentage_head_tail."%\n";
+$percentage_tail_head = ($tail_head/$no_of_times_flip)*100;
+echo "Percentage of TH:".$percentage_tail_head."%\n";
+$percentage_tail_tail = ($tail_tail/$no_of_times_flip)*100;
+echo "Percentage of TT:".$percentage_tail_tail."%\n";
 
 ?>
